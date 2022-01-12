ca9542eb0f0e3502f5cea5d161fbacaa2493f40f
Pål Nødseth <pal.nodseth@eggsdesign.com>
2022-01-12 23:30:01 +0100

    Fix some keyboard positioning on mobile

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,13 +5,13 @@
 	let getHint: () => void;
 </script>
 
-<main class="flex flex-col h-screen pb-4 max-w-md m-auto">
+<main class="flex flex-col md:h-screen pb-4 max-w-md m-auto">
 	<div class="h-16  mb-4 md:mb-12  flex flex-initial">
 		<Header>
 			<button on:click={getHint}>Click</button>
 		</Header>
 	</div>
-	<div class="flex-1">
+	<div class="flex-1 pl-4 pr-4">
 		<GameBoard bind:getHint />
 	</div>
 </main>
